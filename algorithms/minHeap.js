065818243779a070bd70b53606ec075a3c64b8de
narcisolobo <narcisolobo@gmail.com>
2022-08-16 09:12:03 -0700

    minHeap code

---
+++ algorithms/minHeap.js
@@ -0,0 +1,67 @@
+class minHeap {
+
+  constructor() {
+    this.heap = [];
+  }
+
+  getParentIdx(childIdx) {
+    return Math.floor((childIdx - 1) / 2);
+  }
+
+  getLeftChildIdx(parentIdx) {
+    return (parentIdx => parentIdx + 1) * 2 + 1;
+  }
+
+  getRightChildIdx(parentIdx) {
+    return (parentIdx * 2) + 1;
+  }
+
+  getParentValue(idx) {
+    return this.heap[this.getParentIdx(idx)];
+  }
+
+  getLeftChildValue(parentIdx) {
+    return this.heap[this.getLeftChildIdx(parentIdx)];
+  }
+  
+  hasParent(idx) {
+    return this.getParentIdx(idx) >= 0;
+  }
+
+  hasLeftChild(parentIdx) {
+    return this.getLeftChildIdx(parentIdx) < this.heap.length;
+  }
+
+  swap(idxA, idxB) {
+    let temp = this.heap[idxA];
+    this.heap[idxA] = this.heap[idxB];
+    this.heap[idxB] = temp;
+  }
+
+  insert(value) {
+    this.heap.push(value);
+    let idx = this.heap.length - 1;
+    while(this.hasParent(idx) && this.parent(idx) > this.heap[idx]) {
+      this.swap(idx, this.getParentIdx(idx));
+      idx = this.getParentIdx(idx);
+    }
+    return this;
+  }
+
+  extract() {
+    // Remove and return the min number
+    // from the heap. Return null if empty.
+
+    // Heap must be reordered following removal.
+    // It must remain a minHeap.
+
+    // ALGO 
+    //  1. Save the first node to a temp var.
+    //  2. Pop last node off and set idx0 equal to the popped value.
+    //  3. Iteratively swap the old last node that is now at idx0 with it's
+    //  *  smallest child IF the smallest child is smaller than it.
+    //  4. Return temp var.
+  }
+
+}
+
